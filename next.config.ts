92bfdf4c80ddc5af9760e89187aa3386bfb308f2
ianadamskelly <40513693+ianadamskelly@users.noreply.github.com>
2026-07-01 21:33:41 +0300

    Redirect non-www domain to www

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,16 @@ import path from "node:path";
 const nextConfig: NextConfig = {
   output: "standalone",
   outputFileTracingRoot: path.join(__dirname),
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "cvrank.kuzakizazi.com" }],
+        destination: "https://www.cvrank.kuzakizazi.com/:path*",
+        permanent: true
+      }
+    ];
+  },
   experimental: {
     serverActions: {
       bodySizeLimit: "10mb"
